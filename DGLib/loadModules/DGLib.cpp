f8e85128bacc097f06aac8daa647df4a3364b866
WangJun <445212619@qq.com>
2019-07-25 14:21:28 +0800

    1.小版本发布

--- DGLib/loadModules/DGLib.cpp
+++ DGLib/loadModules/DGLib.cpp
@@ -16,7 +16,7 @@
 
 PYBIND11_MODULE(DGLib, m) {
 
-	m.def("version", []() {	return "version 0.0.1"; });
+	m.def("version", []() {	return "version 0.0.2"; });
 
 // --- Add bindings here ------------------------------------------------------------------
 
